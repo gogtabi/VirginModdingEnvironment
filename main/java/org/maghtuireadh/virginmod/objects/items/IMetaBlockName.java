992f15f0f93888eee955ffbdf231b8cb34f0a146
LunariusH <lunariushaberdash@gmail.com>
2017-10-03 00:21:27 -0400

    Testing

--- main/java/org/maghtuireadh/virginmod/objects/items/IMetaBlockName.java
+++
@@ -1,7 +0,0 @@
-package org.maghtuireadh.virginmod.objects.items;
-
-import net.minecraft.item.ItemStack;
-
-public interface IMetaBlockName {
-	String getSpecialName(ItemStack stack);
-}
--- main/java/org/maghtuireadh/virginmod/util/Reference.java
+++
@@ -1,9 +0,0 @@
-package org.maghtuireadh.virginmod.util;
-
-public class Reference {
-	public static final String MODID = "vme";
-	public static final String NAME = "Virgin Mod";
-	public static final String VERSION = "1.0";
-	public static final String CLIENT = "org.maghtuireadh.virginmod.proxy.ClientProxy";
-	public static final String COMMON = "org.maghtuireadh.virginmod.proxy.CommonProxy";
-}
