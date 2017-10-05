9154a18f6514cc211a663b709e5243672a3854dc
LunariusH <lunariushaberdash@gmail.com>
2017-10-05 02:17:51 -0400

    Added ATDPumpkin, but no code for it yet.

--- main/java/org/maghtuireadh/virginmod/init/BlockInit.java
+++ main/java/org/maghtuireadh/virginmod/init/BlockInit.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 import org.maghtuireadh.virginmod.objects.blocks.BlockBase;
 import org.maghtuireadh.virginmod.objects.blocks.furnaces.BlockFirepit;
+import org.maghtuireadh.virginmod.objects.blocks.torches.BlockATDLitPumpkin;
+import org.maghtuireadh.virginmod.objects.blocks.torches.BlockATDPumpkin;
 import org.maghtuireadh.virginmod.objects.blocks.torches.BlockATDTorch;
 
 import net.minecraft.block.Block;
@@ -21,5 +23,7 @@ public class BlockInit {
 	public static final Block BLOCK_COPPER = new BlockBase("block_copper", Material.IRON);
 	public static final Block ATD_TORCH = new BlockATDTorch("block_atd_torch");
 	public static final BlockFirepit BLOCK_FIREPIT = new BlockFirepit("block_firepit", Material.ROCK);
+	public static final BlockATDPumpkin BLOCK_ATD_PUMPKIN = new BlockATDPumpkin("block_atd_pumpkin", Material.GOURD);
+	public static final BlockATDLitPumpkin BLOCK_ATD_LIT_PUMPKIN = new BlockATDLitPumpkin("block_atd_pumpkin", Material.GOURD);
 //	public static final BlockFirepit2 ADV_FIREPIT = new BlockFirepit2("advanced_firepit");
 }
---
+++ main/java/org/maghtuireadh/virginmod/objects/blocks/torches/BlockATDPumpkin.java
@@ -0,0 +1,31 @@
+package org.maghtuireadh.virginmod.objects.blocks.torches;
+
+import org.maghtuireadh.virginmod.init.BlockInit;
+
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Predicate;
+
+import net.minecraft.block.BlockPumpkin;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+
+public class BlockATDPumpkin extends BlockPumpkin{
+	public BlockATDPumpkin(String string, Material gourd) {
+	}
+
+/*	private static final Predicate<IBlockState> IS_PUMPKIN = new Predicate<IBlockState>()
+			{
+				public boolean apply(@Nullable IBlockState state)
+		        {
+		            return state!= null && (state.getBlock() == BlockInit.BLOCK_ATD_PUMPKIN || state.getBlock() == BlockInit.BLOCK_ATD_LIT_PUMPKIN);
+		        }
+    };*/
+    
+}
+
